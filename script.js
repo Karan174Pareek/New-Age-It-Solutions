41e264ec7aa7bcca582ed683b31b17edda7771c6
Karan174Pareek <143249558+Karan174Pareek@users.noreply.github.com>
2024-11-09 18:42:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 function connectToWhatsApp() {
     window.location.href = "https://wa.me/8101482088";  // Update with your WhatsApp number
 }
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('show');
+}
